eb90e2d30a15fc0127638cb3a9b83110fe38a146
leonardoA-Silva <leonardoalves27silva@gmail.com>
2026-09-09 21:04:19 -0300

    refactor(header): point signup button to /register route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,7 +59,7 @@ export default function Header() {
           <Link
             className={styles.botaoCadastrar}
             style={{ textDecoration: "none", color: "white" }}
-            href="/cadastro"
+            href="/register"
           >
             Cadastrar
           </Link>
